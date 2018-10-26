4225827026aba262c954c2a9c57c69c91284cbf7
Anwesh43 <anweshthecool0@gmail.com>
2018-10-26 11:34:51 +0530

    adding TickView class

--- tickview/src/main/java/com/anwesh/uiprojects/tickview/TickView.kt
+++ tickview/src/main/java/com/anwesh/uiprojects/tickview/TickView.kt
@@ -33,3 +33,21 @@ fun Canvas.drawTickView(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class TickView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                
+            }
+        }
+        return true
+    }
+}
