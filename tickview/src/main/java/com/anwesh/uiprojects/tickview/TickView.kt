55f7d472bf05d447dd009d6c408a768b10bbed2a
Anwesh43 <anweshthecool0@gmail.com>
2018-10-26 11:44:10 +0530

    adding TickNode class

--- tickview/src/main/java/com/anwesh/uiprojects/tickview/TickView.kt
+++ tickview/src/main/java/com/anwesh/uiprojects/tickview/TickView.kt
@@ -98,4 +98,49 @@ class TickView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TickNode(var i : Int, val state : State = State()) {
+
+        private var next : TickNode? = null
+
+        private var prev : TickNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = TickNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTickView(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TickNode {
+            var curr : TickNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
