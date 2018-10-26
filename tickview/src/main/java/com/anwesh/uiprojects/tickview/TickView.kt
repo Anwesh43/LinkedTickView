f495448ebd8c767e0b7e699f717c0b1c92ff1b3f
Anwesh43 <anweshthecool0@gmail.com>
2018-10-26 11:23:43 +0530

    importing libraries and defining number of nodes

--- tickview/src/main/java/com/anwesh/uiprojects/tickview/TickView.kt
+++ tickview/src/main/java/com/anwesh/uiprojects/tickview/TickView.kt
@@ -4,3 +4,11 @@ package com.anwesh.uiprojects.tickview
  * Created by anweshmishra on 26/10/18.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.content.Context
+
+val nodes : Int = 5
