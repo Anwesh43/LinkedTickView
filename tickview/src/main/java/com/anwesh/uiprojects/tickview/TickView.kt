9690acb5f9cb7a8cd2f3b7ea15e53a6a3917bedf
Anwesh43 <anweshthecool0@gmail.com>
2018-10-26 11:37:16 +0530

    adding State class

--- tickview/src/main/java/com/anwesh/uiprojects/tickview/TickView.kt
+++ tickview/src/main/java/com/anwesh/uiprojects/tickview/TickView.kt
@@ -45,9 +45,29 @@ class TickView(ctx : Context) : View(ctx) {
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-                
+
             }
         }
         return true
+    }
+
+    data class State(var scale : Float = 0f, var dir : Float = 0f, var prevScale : Float = 0f) {
+
+        fun update(cb : (Float) -> Unit) {
+            scale += 0.05f * dir
+            if (Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                cb(prevScale)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1f - 2 * prevScale
+                cb()
+            }
+        }
     }
 }
